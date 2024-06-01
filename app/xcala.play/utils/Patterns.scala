3455f260f6a41e405c976a87d8a3433f711eddf1
keivanabdi <keivan.a.khorsand@gmail.com>
2024-06-01 13:42:42 +0000

    validation improvements

---
+++ app/xcala.play/utils/Patterns.scala
@@ -0,0 +1,12 @@
+package xcala.play.utils
+
+import scala.util.matching.Regex
+
+object Patterns {
+
+  val urlPattern: Regex =
+    """[(http(s)?):\/\/(www\.)?a-zA-Z0-9:%._\+~#=]{2,256}\.[a-z]{1,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)""".r
+
+  val accountUsernamePattern: Regex = ("(" + urlPattern.toString + ")?" + """[a-z0-9_\.]{3,20}""").r
+
+}
